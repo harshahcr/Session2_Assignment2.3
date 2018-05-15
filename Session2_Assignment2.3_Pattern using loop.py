9247bd84693bb78930772a063d5adb7fcfcd5ae5
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-16 00:55:25 +0530

    Delete Session2_Assignment2.3_Pattern using loop.py

--- Session2_Assignment2.3_Pattern using loop.py
+++
@@ -1,7 +0,0 @@
-#Reverse of word from user input
-
-userinput = str(input('Hi, Please input your words to be displayed in reverse and have fun - '))
-print('Your input :',userinput)
-#index and slice notation
-reverse = userinput[::-1]
-print('The backwards value of the input :',reverse)
